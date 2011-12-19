638078be925da08ccad7359b2d4a30936373f474
Evgeniy Serykh <e.v.serykh@gmail.com>
2011-12-19 15:33:03 +0700

    Switched locale to ru

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,3 +1,5 @@
 class Paper < ActiveRecord::Base
   validates_presence_of :kind, :authority, :title, :published_on
+
+  has_enum :kind
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -5,4 +5,10 @@ describe Paper do
   it { should validate_presence_of :authority }
   it { should validate_presence_of :title }
   it { should validate_presence_of :published_on }
+
+  it { should allow_value('decision').for(:kind) }
+  it { should allow_value('direction').for(:kind) }
+  it { should allow_value('order').for(:kind) }
+
+  it { should_not allow_value('blablabla').for(:kind) }
 end
