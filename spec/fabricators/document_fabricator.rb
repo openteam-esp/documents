9e98d8714186d510f7eca5a3816a34957ce49ca4
Evgeny Lapin <lev@openteam.ru>
2012-01-16 15:04:22 +0700

    fix tests

--- spec/fabricators/document_fabricator.rb
+++ spec/fabricators/document_fabricator.rb
@@ -4,4 +4,5 @@ Fabricator(:document) do
   title         'title'
   number        '123123'
   approved_on   '2011-12-19'
+  file_url      '/public/1.doc'
 end
--- spec/fabricators/project_fabricator.rb
+++ spec/fabricators/project_fabricator.rb
@@ -3,4 +3,5 @@ Fabricator(:project) do
   authority     'authority'
   title         'title'
   published_on  '2011-12-19'
+  file_url      '/public/1.doc'
 end
