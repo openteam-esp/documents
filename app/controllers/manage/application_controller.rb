aa30195999a53324926a76f6908040db09a4f840
Dmitry Lihachev <lda@openteam.ru>
2012-01-24 15:36:31 +0700

    admin -> manage

--- app/controllers/manage/application_controller.rb
+++ app/controllers/manage/application_controller.rb
@@ -1,3 +1,6 @@
 class Manage::ApplicationController < InheritedResourcesController
+
+  before_filter :authenticate_user!
+
   before_filter proc { |controller| (controller.action_has_layout = false) if controller.request.xhr? }
 end
