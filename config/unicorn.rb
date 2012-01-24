3b795a2c88df6f8d03489747b40aae9ab78c2de2
Dmitry Lihachev <lda@openteam.ru>
2012-01-24 17:12:40 +0700

    /tmp подчищается на перезагрузках

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 worker_processes 2
 preload_app true
 timeout 300
-listen "/tmp/unicorn/esp/documents.sock", :backlog => 64
+listen "/tmp/unicorn-esp-documents.sock", :backlog => 64
 pid "/var/run/unicorn/esp/documents.pid"
 stderr_path "/var/log/unicorn/esp/documents.stderr.log"
 stdout_path "/var/log/unicorn/esp/documents.stdout.log"
