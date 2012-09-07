fcba47a18d1134c18f0c57fba1d6f542c747288c
Evgeniy Serykh <e.v.serykh@gmail.com>
2012-09-07 16:00:27 +0700

    fix enum values

--- app/models/document.rb
+++ app/models/document.rb
@@ -14,7 +14,7 @@ class Document < Paper
 
   after_save :send_add_message
 
-  has_enum :kind, %w[decision direction project]
+  has_enum :kind, %w[decision direction order]
 
   alias :assertations :assertations_for_project
   alias :cancels      :cancels_for_documents
