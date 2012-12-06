4996664450590142f4fa825c0e945c53da277883
tab <tukhfatullin@gmail.com>
2012-12-06 10:13:46 +0700

    Fix create users migration

--- db/migrate/20120124092939_sso_create_users.rb
+++ db/migrate/20120124092939_sso_create_users.rb
@@ -13,7 +13,14 @@ class SsoCreateUsers < ActiveRecord::Migration
                 :phone,
                 :urls
       t.text    :raw_info       # omniauth[:extra]
-      t.trackable
+
+      # Trackable
+      t.integer  :sign_in_count
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+
       t.timestamps
     end
 
