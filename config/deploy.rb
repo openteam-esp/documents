1ce51cfaeabe3b9e1842e179d8ce6fb65699cfca
Evgeny Lapin <lev@openteam.ru>
2015-12-11 15:21:01 +0600

    added sunspot.yml to shared in production

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,6 @@
-set :default_stage, :ato
-
 require 'openteam/capistrano/recipes'
 require 'whenever/capistrano'
+
+set :default_stage, :ato
+
+set :shared_children, fetch(:shared_children) + %w[config/sunspot.yml]
